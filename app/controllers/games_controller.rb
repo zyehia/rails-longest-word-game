7f367461088dff74158e5b9fd255527c9eedda82
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-09 14:57:13 +0000

    The logic is working and the user can play again

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
       @letters = []
@@ -8,5 +11,9 @@ class GamesController < ApplicationController
     @letters = params[:letters].split(" ")
     @word = params[:word]
     @word_in_grid = @word.split("").all? {|letter| @letters.include? letter.upcase }
+    
+    raw_data = open("https://wagon-dictionary.herokuapp.com/#{@word}").read
+    json_data = JSON.parse(raw_data)
+    @word_is_real = json_data["found"]
   end
 end
