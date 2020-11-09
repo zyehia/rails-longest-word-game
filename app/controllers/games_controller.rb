631a55fed38c50e712f6dac2b57b29b7f282ec39
Ziyad Yehia <ziyadyehia93@gmail.com>
2020-11-09 14:41:10 +0000

    Can now check whether a word is in the grid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,13 @@ class GamesController < ApplicationController
   def new
       @letters = []
       10.times { @letters << ("A".."Z").to_a[rand(26)] }
-      puts @letters
   end
 
   def score
+    @letters = params[:letters].split(" ")
+    p @letters
+    @word = params[:word]
+    p @word
+    @error = !@word.split("").all? {|letter| @letters.include? letter.upcase }
   end
 end
